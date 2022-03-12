e623ef37db65f9513f4831cff71b367f68456da9
Zarvah <mznauval.priadi@gmail.com>
2022-03-12 18:04:14 +0700

    menambahkan kode buah

--- modull3_1302203114/KodeBuah.cs
+++ modull3_1302203114/KodeBuah.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace modull3_1302203114
 {
-    internal class KodeBuah
+    public class KodeBuah
     {
+        public enum Buah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa ,Jagung, Kurma, Durian, Anggur, Melon, Semangka}
+        public string Kode(Buah buah)
+        {
+            string[] kode = { "A00", "BOO", "COO", "DOO", "EOO", "FOO", "GOO", "HOO", "IOO", "JOO", "KOO", "LOO", "MOO", "NOO", "OOO" };
+            return kode[(int)buah]; 
+        }
     }
 }
